2db8d65e569a3a3e8e0a0628436618255739f0e6
hazel <129399801+paigefnf@users.noreply.github.com>
2025-04-18 23:48:45 -0400

    Initial commit

---
+++ assets/songs/test/scripts/Cam Zoom On Hit.lua
@@ -0,0 +1,7 @@
+function goodNoteHit()
+	triggerEvent('Add Camera Zoom', 0.010, 0.01)
+end
+
+function opponentNoteHit()
+	triggerEvent('Add Camera Zoom', 0.010, 0.01)
+end
---
+++ source/funkin/game/stages/StageWeek1.hx
@@ -0,0 +1,34 @@
+package funkin.game.stages;
+
+import funkin.game.stages.objects.*;
+import funkin.game.Character;
+
+class StageWeek1 extends BaseStage
+{
+	override function create()
+	{
+		var bg:BGSprite = new BGSprite('stages/week1/stageback', -600, -200, 0.9, 0.9);
+		add(bg);
+
+		var stageFront:BGSprite = new BGSprite('stages/week1/stagefront', -650, 600, 0.9, 0.9);
+		stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
+		stageFront.updateHitbox();
+		add(stageFront);
+		if(!ClientPrefs.data.lowQuality) {
+			var stageLight:BGSprite = new BGSprite('stages/week1/stage_light', -125, -100, 0.9, 0.9);
+			stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
+			stageLight.updateHitbox();
+			add(stageLight);
+			var stageLight:BGSprite = new BGSprite('stages/week1/stage_light', 1225, -100, 0.9, 0.9);
+			stageLight.setGraphicSize(Std.int(stageLight.width * 1.1));
+			stageLight.updateHitbox();
+			stageLight.flipX = true;
+			add(stageLight);
+
+			var stageCurtains:BGSprite = new BGSprite('stages/week1/stagecurtains', -500, -300, 1.3, 1.3);
+			stageCurtains.setGraphicSize(Std.int(stageCurtains.width * 0.9));
+			stageCurtains.updateHitbox();
+			add(stageCurtains);
+		}
+	}
+}
